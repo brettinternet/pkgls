c05e823d93f8804f360910b66e52f89e923d075d
brett <brettinternet@gmail.com>
2021-07-02 18:07:18 -0600

    feat: modify io to support filename and list

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,9 @@
+use crate::error::*;
 use std::ffi::OsStr;
 use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::fs::OpenOptions;
+use std::io::{stdout, Write};
+use std::io::{BufRead, BufReader, BufWriter};
 use std::path::Path;
 
 /// TODO: support toml, yml, json
@@ -29,9 +33,35 @@ pub struct Output<'a> {
 
 impl<'a> Output<'a> {
     pub fn new(filename: Option<&'a str>) -> Self {
-        Self {
-            format: parse_output_format(filename),
-            filename,
+        match filename {
+            Some(filename) => Self {
+                format: parse_output_format(filename),
+                filename: Some(filename),
+            },
+            None => Self {
+                format: OutputFormat::Stdout,
+                filename: None,
+            },
+        }
+    }
+
+    pub fn write(&self, content: String, force: bool) -> Result<()> {
+        match (self.format, self.filename) {
+            (OutputFormat::Txt, Some(filename)) => {
+                let file = OpenOptions::new()
+                    .create_new(!force)
+                    .read(force)
+                    .write(true)
+                    .create(force)
+                    .open(Path::new(filename))?;
+                let mut writer = BufWriter::new(file);
+                writer.write_all(content.as_bytes())?;
+                Ok(())
+            }
+            _ => {
+                writeln!(stdout(), "{}", content)?;
+                Ok(())
+            }
         }
     }
 }
@@ -53,86 +83,102 @@ impl Display for InputFormat {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+fn read<'a>(filename: &'a str) -> Result<Vec<String>> {
+    let file = OpenOptions::new().read(true).open(Path::new(filename))?;
+    let reader = BufReader::new(file);
+    let mut file_lines: Vec<String> = Vec::new();
+    reader.lines().for_each(|line| {
+        let line = line.unwrap_or_default();
+        let first_char = line.chars().next();
+        if first_char != "#".chars().next() && !line.is_empty() {
+            file_lines.push(line);
+        }
+    });
+    Ok(file_lines)
+}
+
+#[derive(Debug, Clone)]
 pub struct Input<'a> {
     /// Input format type
     pub format: InputFormat,
 
     /// Files to read from
     pub filename: Option<&'a str>,
+
+    /// Package list to read from
+    pub list: Vec<String>,
 }
 
 /// TODO: implement input
 #[allow(dead_code)]
 impl<'a> Input<'a> {
-    pub fn new(filename: Option<&'a str>) -> Self {
-        Self {
+    pub fn from_file(filename: &'a str) -> Result<Self> {
+        let list = read(filename)?;
+        let input = Self {
             format: parse_input_format(filename),
-            filename,
+            filename: Some(filename),
+            list,
+        };
+        Ok(input)
+    }
+
+    pub fn from_list(list: Vec<String>) -> Self {
+        Self {
+            format: InputFormat::Stdin,
+            filename: None,
+            list,
         }
     }
 }
 
-fn get_extension_from_filename(filename: Option<&str>) -> Option<&str> {
-    if let Some(filename) = filename {
-        Path::new(filename).extension().and_then(OsStr::to_str)
-    } else {
-        None
-    }
+fn get_extension_from_filename(filename: &str) -> Option<&str> {
+    Path::new(filename).extension().and_then(OsStr::to_str)
 }
 
-fn parse_output_format(filename: Option<&str>) -> OutputFormat {
-    match (filename, get_extension_from_filename(filename)) {
-        (Some(_), Some("txt")) => {
+fn parse_output_format(filename: &str) -> OutputFormat {
+    match get_extension_from_filename(filename) {
+        Some("txt") => {
             debug!("Using txt extension found in output filename");
             OutputFormat::Txt
         }
-        (Some(filename), Some(ext)) => {
+        Some(ext) => {
             warn!(
                 "Unsupported output format '{}' for output argument '{}', defaulting to plain text",
                 ext, filename
             );
             OutputFormat::Txt
         }
-        (Some(filename), None) => {
+        None => {
             info!(
                 "No extension found for output argument '{}', defaulting to plain text",
                 filename
             );
             OutputFormat::Txt
         }
-        (None, _) => {
-            debug!("Missing output filename extension, defaulting to stdout");
-            OutputFormat::Stdout
-        }
     }
 }
 
 /// TODO: implement input
 #[allow(dead_code)]
-fn parse_input_format(filename: Option<&str>) -> InputFormat {
-    match (filename, get_extension_from_filename(filename)) {
-        (Some(_), Some("txt")) => {
+fn parse_input_format(filename: &str) -> InputFormat {
+    match get_extension_from_filename(filename) {
+        Some("txt") => {
             debug!("Using txt extension found in input filename");
             InputFormat::Txt
         }
-        (Some(filename), Some(ext)) => {
+        Some(ext) => {
             warn!(
                 "Unsupported output format '{}' for <FILE> argument '{}', defaulting to plain text",
                 ext, filename
             );
             InputFormat::Txt
         }
-        (Some(filename), None) => {
+        None => {
             info!(
                 "No extension found for output argument '{}', defaulting to plain text",
                 filename
             );
             InputFormat::Txt
-        }
-        (None, _) => {
-            debug!("Missing input filename extension, defaulting to stdin");
-            InputFormat::Stdin
         }
     }
 }
